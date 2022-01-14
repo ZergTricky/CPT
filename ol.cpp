8e092c6f1828096aff9efa21efb854e4a5b30e61
ZergTricky <belousov.egor1@yandex.ru>
2022-01-14 20:10:08 +0300

    Added 2 basic templates

---
+++ ol.cpp
@@ -0,0 +1,23 @@
+//This is my template for not multi testing problems
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define flush cout.flush
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pl = pair<ll, ll>;
+const ll INF = 1e9 + 7;
+const ll mod = 1e9 + 7;
+const ll mod2 = 998244353;
+const ld eps = 1e-9;
+const ld PI = acos(-1);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
---
+++ olm.cpp
@@ -0,0 +1,30 @@
+//This my template for problems with multi testing
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define flush cout.flush
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pl = pair<ll, ll>;
+const ll INF = 1e9 + 7;
+const ll mod = 1e9 + 7;
+const ll mod2 = 998244353;
+const ld eps = 1e-9;
+const ld PI = acos(-1);
+
+void solve(){
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)solve();
+    return 0;
+}
